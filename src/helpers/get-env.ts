27b0f6ac47b649457e99c74bd2b705acef1ec5f8
Sergey Svezhenets <ssvezhenets@tradingview.com>
2024-07-05 13:04:58 +0300

    add Barchart scrapper

--- src/helpers/get-env.ts
+++ src/helpers/get-env.ts
@@ -1,8 +1,8 @@
 import { env } from 'process';
 
 export const getNumberEnv = (VAR_NAME: string): number |  null => {
-	if (env.ENV_NAME !== undefined) {
-		const result = Number(env.ENV_NAME);
+	if (env[VAR_NAME] !== undefined) {
+		const result = Number(env[VAR_NAME]);
 
 		if (isNaN(result)) {
 			return null;
